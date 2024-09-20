367818b5530c6f7131f5504ca772ff97d0728484
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-09-20 23:56:46 +0700

    Update user table schema and remove seed data

--- migrations/20240901093851_add_user_table.sql
+++ migrations/20240901093851_add_user_table.sql
@@ -9,8 +9,5 @@ CREATE TABLE users (
     photo_link  TEXT
 );
 
--- insert seed data
-INSERT INTO users (username, password, position, first_name, last_name, photo_link) VALUES ('admin', '$2a$14$u4EmE.nz024ZNq7Bp8lTJ.esuLRT6DentEft.uO/CkXPaP7.aqM22', 'Admin', 'John', 'Doe', 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp');
-
 -- +goose Down
 DROP TABLE users;
